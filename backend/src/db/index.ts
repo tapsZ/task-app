6ddbe8134f8fb47470cdf4281b6a710c50b4f978
Tapiwa Zireva <tapiwazireva@gmail.com>
2025-03-26 00:03:44 +0200

    login and create jwt token

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,8 +1,8 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg"
+import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: "postgresql://postgres:test123@mydb:5432/mydb",
+  connectionString: "postgresql://postgres:test123@db:5432/mydb",
 });
 
-export const db = drizzle(pool)
+export const db = drizzle(pool);
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,11 +3,17 @@ import { db } from "../db";
 import { NewUser, users } from "../db/schema";
 import { eq } from "drizzle-orm";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const authRouter = Router();
 
 interface SignUpBody {
   name: string;
+  email: string;
+  password: string;
+}
+
+interface LoginBody {
   email: string;
   password: string;
 }
@@ -37,6 +43,39 @@ authRouter.post(
     }
   }
 );
+
+authRouter.post(
+  "/login",
+  async (req: Request<{}, {}, LoginBody>, res: Response) => {
+    try {
+      const { email, password } = req.body;
+      const [existingUser] = await db
+        .select()
+        .from(users)
+        .where(eq(users.email, email));
+
+      if (!existingUser) {
+        res.status(400).json({ error: "User does not exists" });
+        return;
+      }
+
+      const isMatch = await bcryptjs.compare(password, existingUser.password);
+      if (!isMatch) {
+        res.status(400).json({ error: "Incorrect Password!" });
+        return;
+      }
+
+      const token = jwt.sign({ id: existingUser.id }, "passwordKey");
+    
+      res.status(200).json({token, ...existingUser});
+    } catch (error) {
+      res.status(500).json({ error: error });
+    }
+  }
+);
+
+
+
 
 authRouter.get("/", (req, res) => {
   res.send("Hey there! from auth");
